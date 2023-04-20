db8e71148be51ae2dd8034a638f2ddcc4c3756e4
hosct <hos@ct.de>
2023-04-20 18:36:02 +0200

    Added C# version.

---
+++ csharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics; // BigInteger et al.
+
+// return the Fibonacci sequence
+static IEnumerable<BigInteger> fib() {
+  (BigInteger prev, BigInteger sum) = (0, 1);
+  while(true) {
+    yield return sum;
+    (prev, sum) = (sum, prev + sum);
+  }
+}
+    
+int limit = 100;
+if(args.Length > 0) {
+  if(int.TryParse(args[0], out int num)) {
+    limit = num;
+  }
+}
+foreach(var f in fib().Take(limit)) {
+  Console.WriteLine($"{f:n0}");
+}
+
