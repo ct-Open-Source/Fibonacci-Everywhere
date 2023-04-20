939d6fc5b9979f7bacc8af77aa6d6a12cc26da5b
Pina Merkert <pmk@ct.de>
2023-04-20 15:55:58 +0200

    Basic recursive code with argparse

---
+++ python/fibonacci.py
@@ -0,0 +1,12 @@
+import argparse
+
+
+def fibonacci(n: int):
+    return fibonacci(n - 1) + fibonacci(n - 2) if n > 1 else n
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Calculate a Fibonacci sequence.')
+    parser.add_argument('length', type=int, help='How long should the sequence be?')
+    args = parser.parse_args()
+    print([fibonacci(n) for n in range(1, args.length)])
