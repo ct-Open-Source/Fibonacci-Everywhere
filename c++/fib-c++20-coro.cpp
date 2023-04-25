c9bf881dcfc24beb9581f48f53a1ce1bd184a2c4
Oliver Lau <oliver@ersatzworld.net>
2023-04-25 16:54:19 +0200

    Simplified C++20 version.

--- c++/fib-c++20-coro.cpp
+++ c++/fib-c++20-coro.cpp
@@ -5,121 +5,80 @@
 // g++ -std=c++20 -O2 -o fib fib-c++20-coro.cpp
 
 template <typename T>
-class [[nodiscard]] generator
+struct generator
 {
-public:
     struct promise_type;
-    using coro_handle_type = std::coroutine_handle<promise_type>;
+    using handle_type = std::coroutine_handle<promise_type>;
 
-private:
-    coro_handle_type coro;
-
-public:
-    explicit generator(coro_handle_type h) : coro{h} {}
-    generator(const generator &) = delete;
-    generator &operator=(const generator &) = delete;
-    generator(generator &&oth) noexcept : coro{std::move(oth.coro)}
-    {
-        oth.coro = nullptr;
-    }
-    generator &operator=(generator &&other) noexcept
-    {
-        if (this != &other)
-        {
-            if (coro)
-            {
-                coro.destroy();
-            }
-            coro = std::move(other.coro);
-            other.coro = nullptr;
-        }
-        return *this;
-    }
-    ~generator()
-    {
-        if (coro)
-        {
-            coro.destroy();
-            coro = nullptr;
-        }
-    }
-
-public:
-    bool has_next()
-    {
-        if (!coro || coro.done())
-        {
-            return false;
-        }
-        coro.resume();
-        return !coro.done();
-    }
-
-    T value()
-    {
-        return coro.promise().current_value;
-    }
-
-public:
-    // implementation of above opaque declaration promise_type
     struct promise_type
     {
-    private:
-        T current_value{};
-        friend class generator;
+        T value_;
+        std::exception_ptr exception_;
 
-    public:
-        promise_type() = default;
-        ~promise_type() = default;
-        promise_type(const promise_type &) = delete;
-        promise_type(promise_type &&) = delete;
-        promise_type &operator=(const promise_type &) = delete;
-        promise_type &operator=(promise_type &&) = delete;
-
-        auto get_return_object()
+        generator get_return_object()
         {
-            return generator{coro_handle_type::from_promise(*this)};
+            return generator(handle_type::from_promise(*this));
         }
+        std::suspend_always initial_suspend() { return {}; }
+        std::suspend_always final_suspend() noexcept { return {}; }
+        void unhandled_exception() { exception_ = std::current_exception(); }
 
-        auto initial_suspend()
+        template <std::convertible_to<T> From>
+        std::suspend_always yield_value(From &&from)
         {
-            return std::suspend_always{};
+            value_ = std::forward<From>(from);
+            return {};
         }
-
-        auto final_suspend() noexcept
-        {
-            return std::suspend_always{};
-        }
-
-        auto return_void()
-        {
-            return std::suspend_never{};
-        }
-
-        auto yield_value(T some_value)
-        {
-            current_value = some_value;
-            return std::suspend_always{};
-        }
-
-        void unhandled_exception()
-        {
-            std::terminate();
-        }
+        void return_void() {}
     };
+
+    handle_type h_;
+
+    generator(handle_type h)
+        : h_(h)
+    {
+    }
+    ~generator() { h_.destroy(); }
+    explicit operator bool()
+    {
+        fill();
+        return !h_.done();
+    }
+    T operator()()
+    {
+        fill();
+        full_ = false;
+        return std::move(h_.promise().value_);
+    }
+
+private:
+    bool full_ = false;
+
+    void fill()
+    {
+        if (!full_)
+        {
+            h_();
+            if (h_.promise().exception_)
+            {
+                std::rethrow_exception(h_.promise().exception_);
+            }
+            full_ = true;
+        }
+    }
 };
 
 template <typename INT>
 generator<INT> fibonacci(unsigned int n)
 {
-    INT prev = 0;
-    INT sum = 1;
-    for (unsigned int i = 1; i < n; ++i)
+    INT a = 0;
+    INT b = 1;
+    for (unsigned int i = 0; i < n; ++i)
     {
-        co_yield sum;
-        INT tmp = prev;
-        prev = sum;
-        sum = tmp + sum;
+        co_yield a;
+        INT sum = a + b;
+        a = b;
+        b = sum;
     }
 }
 
@@ -132,9 +91,9 @@ int main(int argc, char *argv[])
     }
 
     auto seq = fibonacci<uint64_t>(n);
-    while (seq.has_next())
+    while (seq)
     {
-        std::cout << seq.value() << ' ';
+        std::cout << seq() << ' ';
     }
 
     std::cout << std::endl;
