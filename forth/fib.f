e9621914f13d130176850f214c247ec325a9b602
Oliver Lau <oliver@ersatzworld.net>
2023-04-26 12:51:43 +0200

    Added Forth version.

---
+++ forth/fib.f
@@ -0,0 +1,15 @@
+( Print 1st n elements of Fibonacci sequence )
+( run with                                   )
+(   gforth < fib.f                           )
+
+: fib ( n -- )
+cr
+1 1 0        \ start values for sequence
+3 roll       \ pick loop limit
+0 do
+  dup .      \ print current number
+  + dup rot  \ calculate next number
+loop
+drop drop drop ;
+
+93 fib  \ 94th Fibonacci number exceeds 64 bit limit
